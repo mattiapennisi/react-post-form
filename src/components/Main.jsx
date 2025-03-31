ac6761f45d20c06ee437575fc261326e2f8c0224
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-31 12:46:50 +0200

    Add post form with author, title, body, and public options

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,23 @@
+import { useState } from 'react'
+
 export default function Main() {
+
+    const [postForm, setPostForm] = useState({
+        author: '',
+        title: '',
+        body: '',
+        public: false
+    })
+
     return (
         <main id="main">
+            <form>
+                <input type="text" name="authorInput" id="authorInput" />
+                <input type="text" name="titleInput" id="titleInput" />
+                <input type="text" name="bodyInput" id="bodyInput" />
+                <label htmlFor="publicInput">Public</label>
+                <input type="checkbox" name="publicInput" id="publicInput" />
+            </form>
         </main>
     )
 }
